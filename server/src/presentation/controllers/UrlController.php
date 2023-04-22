5c17a2b42fd43f66d61581b90d96c3869c40782b
Wesley0010012 <wglaurindo33@gmail.com>
2023-04-21 23:47:18 -0300

    Create EditorConfigFile

--- server/src/presentation/controllers/UrlController.php
+++ server/src/presentation/controllers/UrlController.php
@@ -11,6 +11,6 @@ use \Src\Presentation\Protocols\UrlController as ProtocolsUrlController;
 class UrlController implements ProtocolsUrlController {
   public function index(HttpRequest $request): HttpResponse {
     if(!$request->body['url'])
-      return HttpHelpers::badRequest(new MissingParamError("url"));
+      return HttpHelpers::badRequest(new MissingParamError('url'));
   }
 }
